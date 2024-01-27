025f18aa4e75eb3820176f4b8b17d3e3763a8abe
nyk <thenykreed@gmail.com>
2024-01-27 14:17:13 -0600

    feather, controller

--- project/scenes/player/player.gd
+++ project/scenes/player/player.gd
@@ -1,27 +1,29 @@
 extends CharacterBody2D
 
-@export var move_speed = 200.0
+@export var move_speed: float = 200.0
 
-#var velocity := Vector2.ZERO
+@export var jump_height: float
+@export var jump_time_to_peak: float
+@export var jump_time_to_descent: float
 
-@export var jump_height : float
-@export var jump_time_to_peak : float
-@export var jump_time_to_descent : float
+@export var friction: float = 50.0
 
-@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
-@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
-@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 @export var feather: Node2D
 @export var feather_impact: float = 1.0
 
 var falling: bool = false
 
-func _physics_process(delta):
+var keyboard_input: bool = true
+
+func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("jump") and not falling:
 		falling = true
 	velocity.y += get_gravity() * delta
-	velocity.x = get_input_velocity() * move_speed
+	velocity.x += get_input_velocity() * move_speed
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		falling = false
@@ -29,15 +31,28 @@ func _physics_process(delta):
 	
 	feather_func_one()
 	move_and_slide()
+	if is_on_floor():
+		velocity.x = move_toward(velocity.x, 0.0, friction)
+	velocity.x = clampf(velocity.x, -move_speed, move_speed)
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		keyboard_input = true
+	if event is InputEventJoypadButton:
+		keyboard_input = false
 
 
 func feather_func_one() -> void:
-	var r1: Vector2 = feather.transform.x
-	feather.look_at(get_global_mouse_position())
-	var r2: Vector2 = feather.transform.x
-	var vel: float = r1.angle_to(r2)
-	
-	velocity += vel * (lerp(r1, r2, 0.5)) * feather_impact
+	var r1: Vector2 = feather.global_transform.x
+	if keyboard_input:
+		feather.look_at(get_global_mouse_position())
+	else:
+		feather.look_at(feather.global_position + Input.get_vector("feather_left", "feather_right", "feather_up", "feather_down"))
+	var r2: Vector2 = feather.global_transform.x
+	var vel: float = abs(r1.angle_to(r2))
+	var pos: Vector2 = lerp(r1, r2, 0.5)
+	velocity -= vel * pos * feather_impact
 
 
 func get_gravity() -> float:
@@ -48,7 +63,7 @@ func get_gravity() -> float:
 	return jump_gravity
 
 
-func jump():
+func jump() -> void:
 	velocity.y = jump_velocity
 
 
