619f5022a21cf74bc9caaef62a234c1f7cf7fda0
nyk <thenykreed@gmail.com>
2024-01-27 22:22:29 -0600

    animation starts

--- project/scenes/creature/creature_visual.gd
+++ project/scenes/creature/creature_visual.gd
@@ -1,14 +1,31 @@
 extends Node2D
 
+@onready var move_legs: AnimatedSprite2D = $Node2D/Feet as AnimatedSprite2D
+@onready var air_legs: AnimatedSprite2D = $Node2D/Body/Feet2 as AnimatedSprite2D
 
-
-func set_velocity(velocity: float, grounded: bool) -> void:
-	if velocity < 1:
+func set_velocity(velocity: Vector2, grounded: bool) -> void:
+	if velocity.x < 1:
 		scale.x = -1
-	if velocity > 1:
+	if velocity.x > 1:
 		scale.x = 1
 	
 	if grounded:
-		pass
+		if velocity.length_squared() < 1.0:
+			move_legs.hide()
+			air_legs.frame = 0
+			air_legs.show()
+		if velocity.x < -1:
+			move_legs.show()
+			air_legs.hide()
+			scale.x = -1
+		if velocity.x > 1:
+			move_legs.show()
+			air_legs.hide()
+			scale.x = 1
 	else:
-		pass
+		move_legs.hide()
+		air_legs.show()
+		if velocity.y > 0:
+			air_legs.frame = 1
+		else:
+			air_legs.frame = 2
